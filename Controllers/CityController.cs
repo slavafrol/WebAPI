db87d28674f0debee97da7185836089cace34b61
sfrolov <sfrolov@CAL1183>
2023-05-15 17:35:15 -0400

    Add project files.

---
+++ Controllers/CityController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly DataContext dc;
+
+        public CityController(DataContext dc)
+        {
+            this.dc = dc;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var cities = dc.Cities.ToArray();
+            return Ok(cities);
+        }
+    }
+}
